719693499696dba60a7df679294a850736314dc6
Akshay Gupta <gupta.akshay01@gmail.com>
2018-03-21 18:33:09 -0400

    image fetched using api for a selected movie...six commit !

--- js/script.js
+++ js/script.js
@@ -32,3 +32,35 @@ function getMovies(movieName){
 			console.log(err);
 		});
 }
+
+/*storing selected movie ID in session storage*/
+function selectedMovie(id){
+	sessionStorage.setItem('movieId', id);
+	window.location = 'movie.html';
+	return false;
+}
+
+/*details of single movie*/
+function getMovie(){
+	let movieId = sessionStorage.getItem('movieId');
+
+	axios.get('http://www.omdbapi.com/?i='+ movieId +'&apikey=ac51e66c')
+		.then((response) => {
+			console.log(response);
+			let movie = response.data;
+			let output = `
+				<div class="row">
+					<div class="col-md-4">
+						<img src="${movie.Poster}" class="thumbnail">
+					<div>
+					<div class="col-md-8">
+					<div>
+				</div>
+			`;
+
+			$('#movie').html(output);
+		})
+		.catch((err) =>{
+			console.log(err);
+		});
+}
